812a66d8b26c81085edb195b6b61273745b791da
Mercy <owinomercy983@gmail.com>
2025-05-01 09:08:31 +0300

    routes lab 6

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,13 +1,14 @@
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-function MovieCard({title}) {
-  
+function MovieCard({ movie }) {
   return (
-    <article>
-        <h2>{title}</h2>
-        {/* What should go here? */}
-    </article>
+    <div>
+      <h2>{movie.title}</h2>
+      <p>Time: {movie.time} minutes</p>
+      <p>Genres: {movie.genres.join(", ")}</p>
+      <Link to={`/movie/${movie.id}`}>View Info</Link>
+    </div>
   );
-};
+}
 
 export default MovieCard;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,33 @@
 import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
+import MovieCard from "../components/MovieCard";
 
 function Home() {
+  const [movies, setMovie] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/movies")
+      .then((r) => r.json())
+      .then((data) => setMovie(data))
+      .catch((error) => console.error(error));
+  }, []);
+
+  const movieList = movies.map((movie) => {
+    return <MovieCard key={movie.id} movie={movie} />;
+  });
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
+        <h1>Home Page</h1>
         {/* Info goes here! */}
+        <h1>{movieList}</h1>
       </main>
     </>
   );
-};
+}
 
 export default Home;
